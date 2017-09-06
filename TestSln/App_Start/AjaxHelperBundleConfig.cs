24b1d3e0c0b1c475b22dc79af84dd90cf8c22861
菅野 勝之 <suganokatsuyuki@KS-MacBook-Pro.local>
2017-09-06 20:30:33 +0900

    ログイン画面からチャット画面に遷移、メッセージ入力出力まで確認

---
+++ TestSln/App_Start/AjaxHelperBundleConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Optimization;
+
+[assembly: WebActivatorEx.PostApplicationStartMethod(typeof(Imamu.App_Start.AjaxHelperBundleConfig), "RegisterBundles")]
+
+namespace Imamu.App_Start
+{
+	public class AjaxHelperBundleConfig
+	{
+		public static void RegisterBundles()
+		{
+			BundleTable.Bundles.Add(new ScriptBundle("~/bundles/ajaxhelper").Include("~/Scripts/jquery.ajaxhelper.js"));
+		}
+	}
+}
---
+++ TestSln/Models/UserModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel;
+
+namespace Imamu.Models
+{
+	public class UserModel
+	{
+
+		/// <summary>
+		/// ユーザID
+		/// </summary>
+		[DisplayName("ユーザID")]
+		public string UserId { get; set; }
+
+		/// <summary>
+		/// パスワード
+		/// </summary>
+		[DisplayName("パスワード")]
+		public string Password { get; set; }
+
+		/// <summary>
+		/// 名前
+		/// </summary>
+		[DisplayName("名前")]
+		public string Name { get; set; }
+	}
+}
